6044e084522ff83a231aa8d333fcd4ba02aa71aa
Oleg <oleglutsk21@gmail.com>
2022-12-13 00:46:29 +0200

    Styled 4 blocks on the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 $(document).ready (function () {
     videoHendler();
+
+    /* CUSTOMER REVIEWS slider */
+    $('.customer-reviews__slider').slick({
+        slidesToShow: 2,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        appendArrows: $('.customer-reviews__slider-arrows'),
+        prevArrow: $('.customer-reviews__slider-arrow-left'),
+        nextArrow: $('.customer-reviews__slider-arrow-right'),
+    });
+
+    /* OUR CLIENTS slider */
+    $('.our-clients__slider').slick({
+        slidesToShow: 6,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        appendArrows: $('.our-clients__slider-arrows'),
+        prevArrow: $('.our-clients__slider-arrow-left'),
+        nextArrow: $('.our-clients__slider-arrow-right'),
+    });
+
+    
 });
 
 function videoHendler() {
@@ -8,7 +30,6 @@ function videoHendler() {
     playButton.addEventListener("click", function() {
         video.play();
         playButton.style.display = 'none';
-        video.paused = false;
     });
     video.addEventListener("play", function() {
         playButton.style.display = 'none';
