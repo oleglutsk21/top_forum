ac87493df7908d805d139e74f85b0dd4419dca35
Oleh Zagoruyko <oleglutsk21@gmail.com>
2023-03-17 09:05:45 +0200

    Styled media page & adaptiv footer

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,6 +3,9 @@ window.onload = function() {
     let menu = document.querySelector('.header__nav-menu');
     let menuPosition = document.querySelector('.header-top').offsetHeight + "px";
     let body = document.querySelector('body');
+    let windowWidth = window.innerWidth;
+    let menuLinks = document.querySelectorAll('.header__menu-item-link');
+    let subMenuLinks = document.querySelectorAll('.header__submenu-item-link');
     menuButton.addEventListener('click', function() {
         if (menu.classList.contains('show-menu')) {
             menu.classList.remove('show-menu');
@@ -14,6 +17,28 @@ window.onload = function() {
         }
     });
     window.addEventListener('resize', function(event) {
+        windowWidth = window.innerWidth;
+        menuPosition = document.querySelector('.header-top').offsetHeight + "px";
+        if (windowWidth > 767.98) {
+            menu.classList.remove('show-menu');
+            body.style.setProperty('overflow', 'auto');
+        }
     }, true);
-    console.log(menuPosition);
+    menuLinks.forEach(function(link){
+        link.addEventListener('click', function(event){
+            if (windowWidth < 767.98) {
+                menu.classList.remove('show-menu');
+                body.style.setProperty('overflow', 'auto');
+            }
+        });
+    });
+    subMenuLinks.forEach(function(link){
+        link.addEventListener('click', function(event){
+            if (windowWidth < 767.98) {
+                menu.classList.remove('show-menu');
+                body.style.setProperty('overflow', 'auto');
+            }
+        });
+    });
+    console.log(subMenuLinks);
 }
